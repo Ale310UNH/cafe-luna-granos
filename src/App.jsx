67ea238038ce0f9dbbc9814b822c3d3825af7125
Ale310UNH <alejonicolas.cozzolino@estudiantes.unahur.edu.ar>
2025-10-02 16:03:33 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './componentes/header/Header'
+import Footer from './componentes/footer/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
