5f3fde63814753f78bea2079f74ae38c90b886aa
Marcelo Fabián López <marcelofabian.lopez@estudiantes.unahur.edu.ar>
2025-10-08 16:58:45 -0300

    correcion ancho de formulario

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,6 @@
 import { Container, Form, Button, Alert } from "react-bootstrap";
 import { useState } from "react";
+import './contact.css'
 
 export default function Contact() {
   const [form, setForm] = useState({ name: "", email: "", date: "", time: "", message: "" });
@@ -18,32 +19,32 @@ export default function Contact() {
   };
 
   return (
-    <Container className="my-4">
+    <Container className="my-4" id="form">
       <h2>Contacto / Reservas</h2>
       {sent && <Alert variant="success">Tu consulta/reserva fue enviada correctamente.</Alert>}
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3">
-          <Form.Label>Nombre</Form.Label>
+          <Form.Label className="d-flex justify-content-start">Nombre</Form.Label>
           <Form.Control name="name" value={form.name} onChange={handleChange} required />
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label>Email</Form.Label>
+          <Form.Label className="d-flex justify-content-start">Email</Form.Label>
           <Form.Control type="email" name="email" value={form.email} onChange={handleChange} required />
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label>Fecha</Form.Label>
+          <Form.Label className="d-flex justify-content-start">Fecha</Form.Label>
           <Form.Control type="date" name="date" value={form.date} onChange={handleChange} required />
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label>Hora</Form.Label>
+          <Form.Label className="d-flex justify-content-start">Hora</Form.Label>
           <Form.Control type="time" name="time" value={form.time} onChange={handleChange} required />
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label>Mensaje / Detalles</Form.Label>
+          <Form.Label className="d-flex justify-content-start">Mensaje / Detalles</Form.Label>
           <Form.Control as="textarea" rows={3} name="message" value={form.message} onChange={handleChange} />
         </Form.Group>
 
